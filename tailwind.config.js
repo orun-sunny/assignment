943ff966b4d14dd3babfbcf977ba4a1bf2f74d37
orun sunny <sunny.mukut@gmail.com>
2024-03-08 01:43:39 +0600

    working on resumen

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
         'hero-pattern': "url('./src/assets/Mask group.png')",
       },
       colors: {
-        backgroud: '#171B1A'
+        backgroud: '#171B1A',
+        'yellow': '#F89222',
+        customGray: '#8F8F8F',
       }
     },
   },
